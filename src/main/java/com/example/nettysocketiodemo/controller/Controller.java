5298a5d4b022270e1c3ac4eaffae359a296a5c63
陈骐 <chenqi4@tedu.cn>
2023-01-31 14:44:53 +0800

    support crossOrigin

--- src/main/java/com/example/nettysocketiodemo/controller/Controller.java
+++ src/main/java/com/example/nettysocketiodemo/controller/Controller.java
@@ -3,6 +3,7 @@ package com.example.nettysocketiodemo.controller;
 import com.example.nettysocketiodemo.service.MessageEventHandler;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/1/30 18:56
  * @description
  */
+@CrossOrigin
 @RestController
 @RequestMapping("demo")
 public class Controller {
